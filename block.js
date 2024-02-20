8a55b56a4e7ef17056a454bebf6441d4e003ec4f
Harris Escalante <11287035+harrisfoes@users.noreply.github.com>
2024-02-20 20:06:28 +0900

    finishe the mouse follow shapes tuts

--- block.js
+++ block.js
@@ -3,18 +3,62 @@ class Block {
     this.x = x;
     this.y = y;
     this.angle = 0;
+    this.c = 70;
   }
 
   display() {
+    noFill();
+    stroke(this.c);
+    push();
     translate(this.x, this.y);
     rotate(this.angle);
-    rect(0, 0, 100, 50);
+
+    if (this.angle > 0 && this.angle < 45) {
+      this.drawRect();
+    } else {
+      this.drawX();
+    }
+    pop();
   }
 
   move() {
-    let distance = dist(mouseX, mouseY, this.x, this.y);
-    if (distance < distMouse) {
-      this.angle += 1;
+    let distance;
+    // if mouse is moving, check distance between mouse and center of square
+    if (pmouseX - mouseX != 0 || pmouseY - mouseY != 0) {
+      distance = dist(mouseX, mouseY, this.x, this.y);
+      if (distance < distMouse) {
+        this.angle += 1;
+        this.c = 255;
+      }
     }
+
+    //if suares are already moving, keep rotating until angle = 90
+    if (this.angle > 0 && this.angle < 90) {
+      this.angle += 1;
+      if (this.c > 70) this.c -= 3;
+    } else {
+      this.angle = 0;
+      this.c = 70;
+    }
+  }
+
+  drawX() {
+    let margin = -size / 2;
+    line(
+      margin + offset / 2,
+      margin + offset / 2,
+      margin + size - offset / 2,
+      margin + size - offset / 2
+    );
+    line(
+      margin + size - offset / 2,
+      margin + offset / 2,
+      margin + offset / 2,
+      margin + size - offset / 2
+    );
+  }
+
+  drawRect() {
+    rect(0, 0, size - offset, size - offset);
   }
 }
--- sketch.js
+++ sketch.js
@@ -1,16 +1,34 @@
-let distMouse = 50;
-let b;
+let distMouse = 15;
+let cols;
+let rows;
+let size = 10;
+let offset = 4;
+let blocks = [];
 
 function setup() {
   createCanvas(400, 400);
   rectMode(CENTER);
   angleMode(DEGREES);
+  cols = width / size;
+  rows = height / size;
 
   b = new Block(width / 2, height / 2);
+
+  for (let i = 0; i < cols; i++) {
+    blocks[i] = [];
+    for (let j = 0; j < rows; j++) {
+      blocks[i][j] = new Block(size / 2 + i * size, size / 2 + j * size);
+    }
+  }
 }
 
 function draw() {
-  background(220);
-  b.move();
-  b.display();
+  background(0);
+
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) {
+      blocks[i][j].move();
+      blocks[i][j].display();
+    }
+  }
 }
